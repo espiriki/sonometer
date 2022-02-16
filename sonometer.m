3cf0e8cabf3dee177e7e619e0de91b0f4d12466c
José Carlos Cazarin Filho <joseespiriki@gmail.com>
2022-02-15 21:12:19 -0700

    Rename sonometro.m to sonometer.m

--- sonometer.m
+++ sonometer.m
@@ -9,26 +9,26 @@ sinal_analise_arquivo = 'ruido.wav';
 
     valor_rms_sinal_calibracao = rms(sinal_calibracao);
     
-    %Como sabemos que um sinal de 94dB equivale a uma pressão rms de 1 Pascal, a
-    %sensibilidade do sistema é S = (valor_rms_sinal_calibracao / 1 pa) =
+    %Como sabemos que um sinal de 94dB equivale a uma pressÃ£o rms de 1 Pascal, a
+    %sensibilidade do sistema Ã© S = (valor_rms_sinal_calibracao / 1 pa) =
     %0.0094
     sensibilidade = rms(sinal_calibracao);
 
     %Analisando o sinal dado
     [sinal_analise, fs_analise] = audioread(sinal_analise_arquivo);
     
-    %Divide o sinal a ser analisado pelo valor de calibração
+    %Divide o sinal a ser analisado pelo valor de calibraÃ§Ã£o
     sinal_analise_calibrado = sinal_analise/sensibilidade;
     
     %Gerando o eixo do tempo do sinal a ser analisado
     eixo_x_tempo = 0 : 1/fs_analise : length(sinal_analise)/fs_analise;
     eixo_x_tempo = eixo_x_tempo(1:end-1);
     
-    %Plotando o gráfico do sinal a ser analisado no tempo
+    %Plotando o grÃ¡fico do sinal a ser analisado no tempo
     figure();
     plot(eixo_x_tempo,sinal_analise_calibrado);
     xlabel('Tempo (s)');
-    ylabel('Pressão (Pa)');
+    ylabel('PressÃ£o (Pa)');
     title('Sinal a ser analisado');
     axis tight;
     grid on;
@@ -38,8 +38,8 @@ sinal_analise_arquivo = 'ruido.wav';
     
     %Imprimindo no console
     fprintf( '\n');
-    fprintf( '------------------------Sonômetro JCCF--------------------\n');
-    fprintf( 'O valor LEQ do sinal passado como parâmetro é de %.2f dB\n', valor_LEQ);
+    fprintf( '------------------------SonÃ´metro JCCF--------------------\n');
+    fprintf( 'O valor LEQ do sinal passado como parÃ¢metro Ã© de %.2f dB\n', valor_LEQ);
     fprintf( '----------------------------------------------------------\n\n');
 
     %Gerando o espectro do sinal a ser analisado
@@ -47,13 +47,13 @@ sinal_analise_arquivo = 'ruido.wav';
  
     [Curva_A] = curva_A(frequencias_espectro);
       
-    %Para podermos fazer a ifft do sinal, precisamos utilizar as frequências
-    %negativas. Então precisamos "espelhar" a curva A para abranger essas
-    %frequências
+    %Para podermos fazer a ifft do sinal, precisamos utilizar as frequÃªncias
+    %negativas. EntÃ£o precisamos "espelhar" a curva A para abranger essas
+    %frequÃªncias
     curva_A_espelhada = [Curva_A Curva_A(end:-1:1)];
 
     %Realizando a FFT do sinal a ser analisado e multiplicando o espectro resultante pela
-    %curva de ponderação
+    %curva de ponderaÃ§Ã£o
     fft_do_sinal = fft(sinal_analise_calibrado);
     espectro_sinal_analise_ponderado_curva_A = fft_do_sinal .* curva_A_espelhada';
       
@@ -66,7 +66,7 @@ sinal_analise_arquivo = 'ruido.wav';
     plot(eixo_x_tempo,[sinal_ponderado_curva_A_tempo sinal_analise_calibrado]);
     title('Sinal antes e depois do A-weighting');
     xlabel('Tempo (s)');
-    ylabel('Pressão (Pa)');
+    ylabel('PressÃ£o (Pa)');
     
     %Calculando a energia do sinal ponderado
     energia_sinal_analise  = sinal_ponderado_curva_A_tempo .^ 2;
@@ -83,19 +83,19 @@ sinal_analise_arquivo = 'ruido.wav';
     title('Energia do sinal ponderado pela curva A - dB');
     xlabel('Tempo (s)');
     
-    %Função que calcula a curva do valor LAS
+    %FunÃ§Ã£o que calcula a curva do valor LAS
     media_temporal(energia_sinal_analise,eixo_x_tempo);
      
-    %Calculando o espectro de terço de oitava do sinal ponderado
+    %Calculando o espectro de terÃ§o de oitava do sinal ponderado
     espectro_terco = espectro_terco_oitava(espectro_sinal_analise_ponderado_curva_A, frequencias_espectro);    
     
     %Valores de frequencia utilizados para plotar o espectro de terco de
     %oitava
     valores_frequencia = [10 12 16 20 25 31.5 40 50 63 80 100 125 160 200 250 315 400 500 630 800 1000 1250 1600 2000 2500 3150 4000 5000 6300 8000 10000 12500 16000];
    
-    %aplica uma compressão logaritimica para diminuir o range no eixo das
-    %frequências e assim as barras do grafico não ficarem muito proximas umas das
-    %outras nas baixas frequências
+    %aplica uma compressÃ£o logaritimica para diminuir o range no eixo das
+    %frequÃªncias e assim as barras do grafico nÃ£o ficarem muito proximas umas das
+    %outras nas baixas frequÃªncias
     xplot = (1.25).^(log10(valores_frequencia)); 
     xplot = xplot'; 
    
@@ -112,8 +112,8 @@ sinal_analise_arquivo = 'ruido.wav';
                    'VerticalAlignment','bottom')
      end
    
-     title ('Espectro de terço de oitava');
-     xlabel('Frequências (Hz)');
+     title ('Espectro de terÃ§o de oitava');
+     xlabel('FrequÃªncias (Hz)');
      ylabel('Magnitude (dB)');
      grid on;
    
